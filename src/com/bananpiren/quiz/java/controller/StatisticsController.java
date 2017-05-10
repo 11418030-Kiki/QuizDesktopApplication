c1decdc48e044c4c06d9b2e11c43d9320490d9d0
Andreas Ekman <info@andreasekman.com>
2017-05-10 15:10:59 +0200

    Added statistics

--- src/com/bananpiren/quiz/java/controller/StatisticsController.java
+++ src/com/bananpiren/quiz/java/controller/StatisticsController.java
@@ -1,7 +1,34 @@
 package com.bananpiren.quiz.java.controller;
 
-/**
- * Created by Andreas on 2017-05-03.
- */
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.chart.PieChart;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TableColumn;
+
 public class StatisticsController {
+
+    ObservableList<PieChart.Data> userData = FXCollections.observableArrayList(
+            new PieChart.Data("Rätt svar", 60),
+            new PieChart.Data("Fel svar", 40)
+    );
+
+
+    @FXML
+    private Tab correctAnswersTab;
+
+    @FXML
+    private PieChart correctAnswersPieChart;
+
+    @FXML
+    private Tab usersTab;
+
+    @FXML
+    private TableColumn<?, ?> quizTableView;
+
+    @FXML
+    private void initialize() {
+        correctAnswersPieChart.setData(userData);
+    }
 }
