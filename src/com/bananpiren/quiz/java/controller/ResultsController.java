446cd79336cc3b4f546485322a145b602431c513
Sovsa <sovsa74@gmail.com>
2017-05-10 20:15:08 +0200

    Fixat Settings.fxml och logik i SettingsController.java och lagt till mer logik i ResultsController.java

--- src/com/bananpiren/quiz/java/controller/ResultsController.java
+++ src/com/bananpiren/quiz/java/controller/ResultsController.java
@@ -1,6 +1,10 @@
 package com.bananpiren.quiz.java.controller;
 
+import com.bananpiren.quiz.Entity.User;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
 
@@ -11,4 +15,22 @@ public class ResultsController {
 
     @FXML
     private TextArea resultsTextArea;
+
+    @FXML
+    private TableColumn quizNameColumn;
+
+    @FXML
+    private TableColumn correctAnswersTable;
+
+    @FXML
+    private TableColumn questionCountColumn;
+
+    @FXML
+    private TableColumn dateColumn;
+
+    final ObservableList<User> data = FXCollections.observableArrayList();
+
+    private void initialize(){
+
+    }
 }
