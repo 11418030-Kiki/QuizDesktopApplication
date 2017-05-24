30e6b435fdbec5af52455ba1c6c01ec7fd9d1955
acroscene <per.ottosson.gbg@gmail.com>
2017-05-24 15:26:16 +0200

    TakeQuiz

---
+++ src/com/bananpiren/quiz/Entity/TakeQuiz.java
@@ -0,0 +1,117 @@
+package com.bananpiren.quiz.Entity;
+
+
+/**
+ * Created by per on 2017-05-23.
+ */
+
+public class TakeQuiz {
+
+    // Quiz
+    private int quizId;
+    private String quizName;
+    private int timeLimit;
+    private String quizStartDate;
+    private String quizEndDate;
+
+    // QuizQuestions
+    private int questionId;
+    private String question;
+    private String questionType;
+
+    // QuestionAnswers
+    private int answerId;
+    private String answer;
+    private int correctAnswer;
+
+    public TakeQuiz() {
+    }
+
+    public int getQuizId() {
+        return quizId;
+    }
+
+    public void setQuizId(int quizId) {
+        this.quizId = quizId;
+    }
+
+    public String getQuizName() {
+        return quizName;
+    }
+
+    public void setQuizName(String quizName) {
+        this.quizName = quizName;
+    }
+
+    public int getTimeLimit() {
+        return timeLimit;
+    }
+
+    public void setTimeLimit(int timeLimit) {
+        this.timeLimit = timeLimit;
+    }
+
+    public String getQuizStartDate() {
+        return quizStartDate;
+    }
+
+    public void setQuizStartDate(String quizStartDate) {
+        this.quizStartDate = quizStartDate;
+    }
+
+    public String getQuizEndDate() {
+        return quizEndDate;
+    }
+
+    public void setQuizEndDate(String quizEndDate) {
+        this.quizEndDate = quizEndDate;
+    }
+
+    public int getQuestionId() {
+        return questionId;
+    }
+
+    public void setQuestionId(int questionId) {
+        this.questionId = questionId;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getQuestionType() {
+        return questionType;
+    }
+
+    public void setQuestionType(String questionType) {
+        this.questionType = questionType;
+    }
+
+    public int getAnswerId() {
+        return answerId;
+    }
+
+    public void setAnswerId(int answerId) {
+        this.answerId = answerId;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    public int getCorrectAnswer() {
+        return correctAnswer;
+    }
+
+    public void setCorrectAnswer(int correctAnswer) {
+        this.correctAnswer = correctAnswer;
+    }
+}
