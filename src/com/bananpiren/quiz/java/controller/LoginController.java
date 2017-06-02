fe520bb23cd8030406801f043232762e4f716974
Eric Andresen <ericandresen91@gmail.com>
2017-06-02 23:42:57 +0200

    Added styling.

--- src/com/bananpiren/quiz/java/controller/LoginController.java
+++ src/com/bananpiren/quiz/java/controller/LoginController.java
@@ -9,6 +9,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 import javax.persistence.EntityManagerFactory;
@@ -27,6 +28,9 @@ public class LoginController {
     @FXML
     private Label informationOutputTextField;
 
+    @FXML
+    private BorderPane loginBordePane;
+
     private MainController mainController = new MainController();
     private Stage primaryStage = new Stage();
     private UserService userService = new UserService();
@@ -34,8 +38,10 @@ public class LoginController {
 
     private static User currentUser;
 
+
     @FXML
     private void initialize() {
+
         userEmailTextField.setText("user@user.se");
         userPasswordTextField.setText("user");
 
