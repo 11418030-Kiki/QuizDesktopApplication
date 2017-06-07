7f24e53a99234d78b2b55ad816a41cf4cf62604c
acroscene <per.ottosson.gbg@gmail.com>
2017-06-07 13:49:40 +0200

    TakeQuiz

--- src/com/bananpiren/quiz/java/controller/StartController.java
+++ src/com/bananpiren/quiz/java/controller/StartController.java
@@ -54,7 +54,9 @@ public class StartController {
     @FXML
     private Button takeQuizButton;
 
-    private boolean runQuiz = false;
+    private boolean runQuiz;
+
+    private boolean quizCorrectExists = true;
 
     private static ArrayList<CheckBox> multiAnswerList = new ArrayList<>();
     private static ArrayList<RadioButton> singleAnswerList = new ArrayList<>();
@@ -66,14 +68,24 @@ public class StartController {
 
     @FXML
     private void initialize() {
+        System.out.println("quizCorrectExists " + quizCorrectExists);
+        // are there tests to correct?
+        for (UserQuiz uq : UserQuizService.getAllUserQuiz()) {
+            if (uq.getPoints() == -1) {
+                System.out.println("uq.getQuizName() = " + uq.getQuizName());
+            }
+        }
 
         // are there tests to correct?
-//        for (Integer uq : UserQuizService.getPointsfromAllQuiz()) {
-//            if (uq == -1) {
-//                new Alert(Alert.AlertType.INFORMATION, "Du har quiz att rätta!").showAndWait();
-//                break;
-//            }
-//        }
+        for (UserQuiz uq : UserQuizService.getAllUserQuiz()) {
+            // alert if you have quiz to correct
+            if ((uq.getPoints() == -1) && quizCorrectExists) {
+                quizCorrectExists = false;
+                new Alert(Alert.AlertType.INFORMATION, "Du har quiz att rätta!").showAndWait();
+
+                break;
+            }
+        }
 
         // Setting data to right column "cellvalue"
         quizNameColumn.setCellValueFactory(new PropertyValueFactory<Quiz, String>("quizName"));
